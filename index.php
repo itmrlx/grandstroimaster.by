32b0d8e25aace34a143e9e87bdfac7310f7c2b7a
itmrlx <itmrlx@gmail.com>
2016-10-07 17:46:46 +0300

    init

---
+++ index.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="img/favicon-160.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/favicon-114.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon-72.png">
+	<link rel="apple-touch-icon-precomposed" href="img/favicon-57.png">
+	<title>Grandstroimaster.by</title>
+	<link rel="stylesheet" href="css/global.css">
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+	<!-- header -->
+	<div class="wrapper header">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-4 logo-container">
+					<img src="img/logo.png" alt="logo">
+					<div class="logo">Grandstroimaster.by</div>
+					<div class="logo-text">Всегда безупречно</div>
+				</div>
+				<div class="col-xs-4 title-container">
+					<div class="title">Комплексный ремонт квартир</div>
+					<div class="title2">Профессиональный ремонт квартир в Минске</div>
+				</div>
+				<div class="col-xs-4 phones-container">
+					<a href="tel:+375297554042" class="tel tel1"><span>+375(29)</span> 755-40-42</a>
+					<a href="tel:+375447340515" class="tel"><span>+375(44)</span> 734-05-15</a>
+					<div class="time">с 9:00 до 20:00 без выходных</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- menu -->
+	<div class="wrapper menu-wr">
+		<div class="container menu-container">
+			<div class="burger-menu">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<ul class="pro-menu">
+				<li><a href="#">Главная</a></li>
+				<li><a href="#">Партнеры</a></li>
+				<li><a href="#">Контакты</a></li>
+				<li><a href="#">Наш инструмент</a></li>
+				<li><a href="#">Наши работы</a></li>
+				<li><a href="#">Статьи</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<!-- main slider -->
+	<div class="container main-slider-container">
+		<div class="main-slider">
+			<div class="slider">
+				<div class="slide" style="background-image: url(img/slide2.jpg);">
+					<div class="text">
+						<h2>Герой Onliner.by</h2>
+						<p>Ремонт квартиры в стиле "Лофт".</p>
+						<a href="https://realt.onliner.by/2014/11/26/kvartira-loft" class="btn btn-primary">Читать подробнее</a>
+					</div>
+				</div>
+				<div class="slide" style="background-image: url(img/slide.jpg);">
+					<div class="text">
+						<h2>Герой Onliner.by</h2>
+						<p>Ремонт квартиры в стиле "Лофт".</p>
+						<a href="https://realt.onliner.by/2014/11/26/kvartira-loft" class="btn btn-primary">Читать подробнее</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- about us -->
+	<div class="container about-us-container">
+		<h2 class="title-block"><span>Наша команда</span></h2>
+		<div class="about-us">
+			<img class="alignleft" src="img/command.jpg" alt="our-team"><p>Команда «Grandstroymaster.by» оказывает услуги по ремонту и отделке квартир и других помещений в Минске с 2005 года.</p>
+			<p>Осуществляем комплексные ремонтные работы, а также оказываем полный спектр услуг по отделке жилых и коммерческих помещений в сегменте новостроек и вторичного жилья.</p>
+			<p>Данным направлением мы занимаемся более 10 лет. За это время нашими сотрудниками успешно реализовано свыше 100 объектов от простых до эксклюзивных. Квалифицированные специалисты компании окажут Вам консультацию и выполнят работы по комплексному ремонту квартиры «под ключ», исходя из Ваших пожеланий и возможностей, в самые короткие сроки и, безусловно, на высоком уровне.</p>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+
+	<!-- portfolio -->
+	<div class="container portfolio-container">
+		<div class="title-block"><span>Наши работы</span></div>
+		<div class="row">
+			<div class="col-xs-3">
+				<a href="#" class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">Дизайн интерьера интерьера</div>
+				</a>
+			</div>
+			<div class="col-xs-3">
+				<a href="#" class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">Дизайн интерьера</div>
+				</a>
+			</div>
+			<div class="col-xs-3">
+				<a href="#" class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">Дизайн интерьера</div>
+				</a>
+			</div>
+			<div class="col-xs-3">
+				<a href="#" class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">Дизайн интерьера</div>
+				</a>
+			</div>
+			<div class="col-xs-3">
+				<a href="#" class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">Дизайн интерьера</div>
+				</a>
+			</div>
+			<div class="col-xs-3">
+				<a href="#" class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">Дизайн интерьера</div>
+				</a>
+			</div>
+			<div class="col-xs-3">
+				<a href="#" class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">Дизайн интерьера</div>
+				</a>
+			</div>
+			<div class="col-xs-3">
+				<a href="#" class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">Дизайн интерьера</div>
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<!-- why we -->
+	<div class="container why-we-container">
+		<div class="title-block"><span>Почему выбирают именно нас</span></div>
+		<div class="why-we">
+			<div class="row">
+				<div class="col-xs-6">
+					<div class="item">
+						<div class="title">Опыт компании более 10 лет</div>
+						<p>Ремонт и отделку квартир наша компания начала выполнять с 2005 года.</p>
+					</div>
+					<div class="item">
+						<div class="title">Соблюдение технологий</div>
+						<p>Все работы производятся в соответствии с техническим регламентом.</p>
+					</div>
+					<div class="item">
+						<div class="title">Специалисты высокого уровня</div>
+						<p>Ремонт и отделку квартир в нашей компании производят только квалифицированные специалисты.</p>
+					</div>
+					<div class="item">
+						<div class="title">Подробные сметы</div>
+						<p>Мы предоставляем подробные сметы, актуальность которых достигает от 90 до 100%.</p>
+					</div>
+				</div>
+				<div class="col-xs-6">
+					<div class="item">
+						<div class="title">Отсутствие скрытых платежей</div>
+						<p>Наличие подробной сметы, является Вашей гарантией от так называемых дополнительных работ.</p>
+					</div>
+					<div class="item">
+						<div class="title">Гарантия на все виды работ</div>
+						<p>Мы гордимся качеством своей работы и предоставляем гарантию на все виды выполненных нами работ.</p>
+					</div>
+					<div class="item">
+						<div class="title">Дополнительные скидки от партнёров</div>
+						<p>Наши партнёры предоставляют материалы по стоимости близкой к закупочным.Подробнее об этом вы можете прочитать здесь.</p>
+					</div>
+					<div class="item">
+						<div class="title">Вежливые сотрудники</div>
+						<p>Отсутсвие хамства и наличие улыбки гарантируем)</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- review -->
+	<div class="container review-container">
+		<div class="title-block"><span>Нам доверяют</span></div>
+		<div class="review">
+			<div class="review-slider">
+				<div class="slide">
+					<a href="img/review.jpg" class="fancybox" data-fancybox-group="review"><img src="img/review.jpg" alt="review"></a>
+				</div>
+				<div class="slide">
+					<a href="img/review.jpg" class="fancybox" data-fancybox-group="review"><img src="img/review.jpg" alt="review"></a>
+				</div>
+				<div class="slide">
+					<a href="img/review.jpg" class="fancybox" data-fancybox-group="review"><img src="img/review.jpg" alt="review"></a>
+				</div>
+				<div class="slide">
+					<a href="img/review.jpg" class="fancybox" data-fancybox-group="review"><img src="img/review.jpg" alt="review"></a>
+				</div>
+				<div class="slide">
+					<a href="img/review.jpg" class="fancybox" data-fancybox-group="review"><img src="img/review.jpg" alt="review"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- princips -->
+	<div class="container about-us-container">
+		<h2 class="title-block"><span>Наши принципы</span></h2>
+		<div class="about-us">
+			<p>Наша команда специализируется на отделке помещений высокого уровня по доступным ценам.
+				Основными  принципами, которыми мы руководствуемся:</p>
+			<p>это четкое соблюдение регламента при проведении работ
+				мы проводим строгую политику против раздувания смет.</p>
+			<p>Так как наша команда является специалистами высокого уровня, это позволяет нам делать качественную работу быстрее и точнее.</p>
+			<p>Мы пользуемся профессиональным инструментом и оборудованием для приготовления различных составов, мы  используем миксеры, а не дешёвые дрели.</p>
+			<p>Самое главное в нашей работе это доставить наибольший комфорт заказчику в проведении строительных и отделочных работ, ведь выбрав правильного исполнителя работ по проекту, тем самым заказчик решает свою главную задачу ремонта своего дома.</p>
+			<p>Это и многое другое позволяет нам быть недосягаемыми для конкурентов вопросах качества и общения с заказчиком и при этом оставаться доступным по цене для Вас! </p> 
+			<div class="clearfix"></div>
+		</div>
+	</div>
+
+	<!-- review -->
+	<div class="container review-container">
+		<div class="title-block"><span>Наши партнеры</span></div>
+		<div class="review">
+			<div class="partner-slider">
+				<div class="slide"><img src="img/BOSH.jpg" alt="partner"></div>
+				<div class="slide"><img src="img/OIKOS_1jpg.jpg" alt="partner"></div>
+				<div class="slide"><img src="img/sHEETROCK.jpg" alt="partner"></div>
+				<div class="slide"><img src="img/sniejka-1.jpg" alt="partner"></div>
+				<div class="slide"><img src="img/tayfun-1.jpg" alt="partner"></div>
+				<div class="slide"><img src="img/VETONIT.jpg" alt="partner"></div>
+				<div class="slide"><img src="img/CAPAROL.jpg" alt="partner"></div>
+				<div class="slide"><img src="img/krAFTOOL.jpg" alt="partner"></div>
+				<div class="slide"><img src="img/dulux.jpg" alt="partner"></div>
+				<div class="slide"><img src="img/gyPROC.jpg" alt="partner"></div>
+			</div>
+		</div>
+	</div>
+
+	<!-- team -->
+	<div class="container team-container">
+		<div class="title-block"><span>Для Вас работают</span></div>
+		<div class="team">
+			<div class="row">
+				<div class="col-xs-4">
+					<div class="item">
+						<img src="img/denus_2.png" alt="Денис титов">
+						<div class="title">Денис Титов</div>
+						<div class="desc">Руководитель проектов</div>
+					</div>
+				</div>
+				<div class="col-xs-4">
+					<div class="item">
+						<img src="img/denus_2.png" alt="Денис титов">
+						<div class="title">Денис Титов</div>
+						<div class="desc">Руководитель проектов</div>
+					</div>
+				</div>
+				<div class="col-xs-4">
+					<div class="item">
+						<img src="img/denus_2.png" alt="Денис титов">
+						<div class="title">Денис Титов</div>
+						<div class="desc">Руководитель проектов</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- portfolio -->
+	<div class="container portfolio-container">
+		<div class="title-block"><span>Жилые комплексы в работе</span></div>
+		<div class="row">
+			<div class="col-xs-4">
+				<div class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">ЖК Рахманинов</div>
+				</div>
+			</div>
+			<div class="col-xs-4">
+				<div class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">ЖК Рахманинов</div>
+				</div>
+			</div>
+			<div class="col-xs-4">
+				<div class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">ЖК Рахманинов</div>
+				</div>
+			</div>
+			<div class="col-xs-4">
+				<div class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">ЖК Рахманинов</div>
+				</div>
+			</div>
+			<div class="col-xs-4">
+				<div class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">ЖК Рахманинов</div>
+				</div>
+			</div>
+			<div class="col-xs-4">
+				<div class="portfolio-block">
+					<img src="img/slide.jpg" alt="portfolio">
+					<div class="title">ЖК Рахманинов</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- footer -->
+	<div class="wrapper footer-b">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6">
+					<div class="title">Мы в социакльных сетях</div>
+					<a href="#" class="so so1"></a>
+					<a href="#" class="so so2"></a>
+					<a href="#" class="so so3"></a>
+				</div>
+				<div class="col-xs-6">
+					<div class="rec">&copy; 2016 Grandstroymaster.by ИП Титов Д.А. УНП 490636942</div>
+				</div>
+				<div class="col-xs-12">
+					<p class="webber">Разработка сайта<a href="http://webber.by"><img src="img/webber-gray.svg" alt="Webber создание сайтов">Webber Studio</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	
+	<script src="js/min/jquery-3.0.0-min.js"></script>
+	<script src="js/min/jquery.fancybox-min.js"></script>
+	<script src="js/min/slick-min.js"></script>
+	<script src="js/min/script-min.js"></script>
+</body>
+</html>
